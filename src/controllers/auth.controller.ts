f34206ad93ca8fdb5fb93cd521f19f3918ceef64
Raafina <raafinuradzani16@gmail.com>
2025-03-05 09:09:58 +0700

    feat: add email SMTP function for registration success

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -30,6 +30,9 @@ const registerValidateSchema = Yup.object({
 
 export default {
   async register(req: Request, res: Response) {
+    /**
+     #swagger.tags = ['Auth']
+     */
     const { fullName, username, email, password, confirmPassword } =
       req.body as unknown as TRegister;
 
@@ -64,6 +67,7 @@ export default {
 
   async login(req: Request, res: Response) {
     /**
+     #swagger.tags = ['Auth']
      #swagger.request.body{
      required: true,
      schema: {$ref: "#/components/schemas/LoginRequest"}
@@ -114,6 +118,7 @@ export default {
 
   async me(req: IReqUser, res: Response) {
     /**
+     #swagger.tags = ['Auth']
      #swagger.security [{
     " beaererAuth": []
      }]
